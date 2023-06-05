d2a7cd541c66ab2f2b37d6115a44cbaf978aa7c4
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-05 19:01:13 +0100

    Create precache-manifest.54ed82efd97a5c4ee391f694ff656faf.js

---
+++ build/precache-manifest.54ed82efd97a5c4ee391f694ff656faf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5e49690951b5d7d53aa6ea103ac2c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a37dfccaaa108017172",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "9e9baf718714463994cc",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "6a37dfccaaa108017172",
+    "url": "/static/js/2.9bb3290f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9bb3290f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9baf718714463994cc",
+    "url": "/static/js/main.c36abd81.chunk.js"
+  },
+  {
+    "revision": "0537a98c19c7b96c1dc5",
+    "url": "/static/js/runtime-main.3ca3b49e.js"
+  }
+]);
