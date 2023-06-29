380c8671942b981b55f8bfc0c0717b9fd752d47d
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-29 17:16:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Welcome from './user_pannels/welcome';
-import User from './user_pannels/user';
-import './App.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-function App() {
-  return (
-    <div >
-      <Router>
-      <Switch>
-      <Route exact path="/" component={Welcome}></Route>
-      <Route path="/user" component={User}></Route>
-      </Switch>
-      </Router>
-     
-    </div>
-  );
-}
+  import React from 'react';
+  import Welcome from './user_pannels/welcome';
+  import User from './user_pannels/user';
+  import './App.css';
+  import AOS from 'aos';
+  import 'aos/dist/aos.css';
+  import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+  function App() {
+    return (
+      <div >
+        <Router>
+        <Switch>
+        <Route exact path="/" component={Welcome}></Route>
+        <Route path="/user" component={User}></Route>
+        </Switch>
+        </Router>
+      
+      </div>
+    );
+  }
 
 
-AOS.init();
-export default App;
+  AOS.init();
+  export default App;
